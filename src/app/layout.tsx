6b731dcba8a7b808175694055ef810a2549d2436
aryannn053 <aryannn053@gmail.com>
2024-09-28 08:41:34 +0530

    fixed .env issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/app/components/Header";
 
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
